4bda89d317d3c0563dc3c166546a7447aceea106
BT_1E10_12 <sample@bths.edu>
2022-05-12 11:07:25 -0400

    Key Map

--- src/Key.java
+++ src/Key.java
@@ -1,7 +1,64 @@
+import org.jnativehook.keyboard.*;
+import java.util.*;
 public class Key {
+
+    public static final Map<Integer,Character> keyMap = new HashMap<>();
+
     private char keyChar;
     private int timesPressed;
     private boolean pressed;
+
+    public static void buildKeyMap()
+    {
+        keyMap.put(NativeKeyEvent.VC_A ,'A');
+        keyMap.put(NativeKeyEvent.VC_B ,'B');
+        keyMap.put(NativeKeyEvent.VC_C ,'C');
+        keyMap.put(NativeKeyEvent.VC_D ,'D');
+        keyMap.put(NativeKeyEvent.VC_E ,'E');
+        keyMap.put(NativeKeyEvent.VC_F ,'F');
+        keyMap.put(NativeKeyEvent.VC_G ,'G');
+        keyMap.put(NativeKeyEvent.VC_H ,'H');
+        keyMap.put(NativeKeyEvent.VC_I ,'I');
+        keyMap.put(NativeKeyEvent.VC_J ,'J');
+        keyMap.put(NativeKeyEvent.VC_K ,'K');
+        keyMap.put(NativeKeyEvent.VC_L ,'L');
+        keyMap.put(NativeKeyEvent.VC_M ,'M');
+        keyMap.put(NativeKeyEvent.VC_N ,'N');
+        keyMap.put(NativeKeyEvent.VC_O ,'O');
+        keyMap.put(NativeKeyEvent.VC_P ,'P');
+        keyMap.put(NativeKeyEvent.VC_Q ,'Q');
+        keyMap.put(NativeKeyEvent.VC_R ,'R');
+        keyMap.put(NativeKeyEvent.VC_S ,'S');
+        keyMap.put(NativeKeyEvent.VC_T ,'T');
+        keyMap.put(NativeKeyEvent.VC_U ,'U');
+        keyMap.put(NativeKeyEvent.VC_V ,'V');
+        keyMap.put(NativeKeyEvent.VC_W ,'W');
+        keyMap.put(NativeKeyEvent.VC_X ,'X');
+        keyMap.put(NativeKeyEvent.VC_Y ,'Y');
+        keyMap.put(NativeKeyEvent.VC_Z ,'Z');
+        keyMap.put(NativeKeyEvent.VC_1, '1');
+        keyMap.put(NativeKeyEvent.VC_2, '2');
+        keyMap.put(NativeKeyEvent.VC_3, '3');
+        keyMap.put(NativeKeyEvent.VC_4, '4');
+        keyMap.put(NativeKeyEvent.VC_5, '5');
+        keyMap.put(NativeKeyEvent.VC_6, '6');
+        keyMap.put(NativeKeyEvent.VC_7, '7');
+        keyMap.put(NativeKeyEvent.VC_8, '8');
+        keyMap.put(NativeKeyEvent.VC_9, '9');
+        keyMap.put(NativeKeyEvent.VC_0, '0');
+        keyMap.put(NativeKeyEvent.VC_OPEN_BRACKET, '[');
+        keyMap.put(NativeKeyEvent.VC_CLOSE_BRACKET ,']');
+        keyMap.put(NativeKeyEvent.VC_BACK_SLASH ,'\\');
+        keyMap.put(NativeKeyEvent.VC_QUOTE , '\"');
+        keyMap.put(NativeKeyEvent.VC_COMMA ,',');
+        keyMap.put(NativeKeyEvent.VC_PERIOD,'.');
+        keyMap.put(NativeKeyEvent.VC_SLASH ,'/');
+        keyMap.put(NativeKeyEvent.VC_SPACE, ' ');
+        keyMap.put(NativeKeyEvent.VC_SEMICOLON ,';');
+        keyMap.put(NativeKeyEvent.VC_MINUS ,'-');
+        keyMap.put(NativeKeyEvent.VC_EQUALS ,'+');
+        
+    }
 
     public Key(char keyChar) {
         this.keyChar = keyChar;
--- src/ProgramWindow.java
+++ src/ProgramWindow.java
@@ -1,7 +1,13 @@
 import javax.swing.*;
 import java.awt.event.*;
 import java.util.*;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+import org.jnativehook.GlobalScreen;
+import org.jnativehook.NativeHookException;
 import org.jnativehook.keyboard.*;
+
 
 
 public class ProgramWindow extends JFrame {
@@ -13,19 +19,35 @@ public class ProgramWindow extends JFrame {
 
     private JFrame j;
     private JLabel label; //test
-    private Map<JLabel,Key> KeyToLabelDisplay;
+    private Map<JPanel,Key> KeyToLabelDisplay;
     private JPanel p; //test
     private JLabel label2; //test
     private JPanel p2; //test
     private KeyManager manager;
+
+
     public ProgramWindow(){
+        Key.buildKeyMap();
+
         j = new JFrame();
         manager = new KeyManager();
         manager.addKey('S');
         manager.addKey('D');
         j.setSize(500, 330);
         j.setLocation(5, 5);
-        j.addKeyListener(new KeyTracker());
+
+
+        // Get the logger for "org.jnativehook" and set the level to off.
+        Logger logger = Logger.getLogger(GlobalScreen.class.getPackage().getName());
+        logger.setLevel(Level.OFF);
+
+        try { // global screen
+            GlobalScreen.registerNativeHook();
+        } catch (NativeHookException e) {
+            e.printStackTrace();
+        }
+        GlobalScreen.addNativeKeyListener(new KeyTracker());
+
 
         p = new JPanel();
         label = new JLabel();
@@ -44,28 +66,42 @@ public class ProgramWindow extends JFrame {
 
         j.setVisible(true);
     }
-    public class KeyTracker extends KeyAdapter { // need even not active window detect keys
+    public class KeyTracker implements NativeKeyListener { // need even not active window detect keys
+
+
         @Override
-        public void keyPressed(KeyEvent e) {
+        public void nativeKeyPressed(NativeKeyEvent e) {
 
                 int key = e.getKeyCode();
+           // System.out.println("pressed");
 
-                if (key == KeyEvent.VK_S) {
+
+                if (key == NativeKeyEvent.VC_S) {
                     manager.pressKey('S');
                     label.setText(manager.getKeyInfo('S'));
-                    System.out.println(manager.getKeyInfo('S'));
+                    //System.out.println(manager.getKeyInfo('S'));
                 }
-                if (key == KeyEvent.VK_D) {
+                if (key == NativeKeyEvent.VC_D) {
                     manager.pressKey('D');
                     label2.setText(manager.getKeyInfo('D'));
-                    System.out.println(manager.getKeyInfo('D'));
+                    //System.out.println(manager.getKeyInfo('D'));
                 }
 
         }
-        public void keyReleased(KeyEvent e) {
+
+
+        @Override
+        public void nativeKeyReleased(NativeKeyEvent e) {
             int key = e.getKeyCode();
-            System.out.println("key released");
+            //System.out.println("key released");
         }
+
+        @Override
+        public void nativeKeyTyped(NativeKeyEvent nativeKeyEvent) {
+            //System.out.println("typed");
+
+        }
+
     }
 }
 
